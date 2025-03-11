45914f6618f600d93c92913910c6dbe0a6799c69
amani <bataganiramani@gmail.com>
2025-03-11 10:36:58 +0100

    structure de base

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
 console.log("Bienvenu dans le Quizz des capitales")
+
+// let nom = prompt("Quel est ton nom ?")
+
+// console.log("C'est parti "+nom+" on y va!")
+  
+let score=0  
+
+for (let chance=0; chance<21; chance++){
+    reponseFinal=prompt(questions[chance]);
+        if (reponseFinal==answers[chance]) {
+            console.log("Bonne réponse")
+        }    
+        else {
+            console.log("Mauvaise reponse !")
+        }
+}
+console.log("Le jeu est terminé")
